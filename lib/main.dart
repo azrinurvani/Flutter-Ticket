879f574dab34c6c6944d75f4a944b2b229573624
azrinurvani <azrinurvani@gmail.com>
2025-04-21 10:11:43 +0700

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_app/base/bottom_nav_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Ticket App",
+      theme: ThemeData(),
+      home: BottomNavBar(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Hello Guys!'),
+    );
+  }
+}
